17c5ef891040d2ea08fc6537a725c8a13de1de81
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-11-13 08:06:27 -0600

    Add basic form example

---
+++ src/app/profile-editor/profile-editor.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InputComponent } from '../shared/input/input.component';
+import { ButtonComponent } from '../shared/button/button.component';
+
+@Component({
+  selector: 'profile-editor-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, InputComponent, ButtonComponent],
+  templateUrl: './profile-editor.component.html',
+  styles: ``,
+})
+export class ProfileEditorComponent implements OnInit {
+  profileForm!: FormGroup;
+  #formBuilder = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.profileForm = this.#formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      address: this.#formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        zip: [''],
+      }),
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.profileForm.value);
+  }
+}
